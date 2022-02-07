6c284b92abb1c1d4d04a4093937a5a0321453f87
Michael J. Klein <lambdamichael@gmail.com>
2022-02-07 17:17:00 -0500

    add first restack test (failing)

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@ mod types;
 pub use types::{Elem, Instructions};
 
 mod parse;
-pub use parse::{parse};
+pub use parse::parse;
 mod executor;
 pub use executor::Executor;
 
@@ -10,51 +10,54 @@ use generic_array::{typenum::U32, GenericArray};
 use sha2::{Digest, Sha256};
 use sha3::{Digest as Sha3_Digest, Sha3_256};
 
-/**
- * Types:
- * - UnsignedInteger
- * - Integer
- * - Float64
- * - Bytes(N)
- * - Multibase
- * - Multihash
- * - Multiaddr
- * - KeyTypes={Ed25519,Secp256k1,Secp256r1,Bls12_381}
- * - PublicKey(KeyType)
- * - PrivateKey(KeyType)
- * - JWT
- * - JWS
- * - JWE
- * - LDP
- * - JSON
- * - CBOR
- *
- * Functions
- * - Sign :: Bytes(N) -> PrivateKey(KeyType) => Bytes(SignatureSize[KeyType])
- * - VerifySignature :: Bytes(N) -> Bytes(SignatureSize[KeyType]) -> PublicKey(KeyType) => Boolean
- * - VerifyRecoveredSignature :: Bytes(N) -> Bytes(SignatureSize[KeyType]) => Boolean
- * - HashSha3_256 :: Bytes(N) => Bytes(32)
- * - Equal
- * - AssertTrue
- *
- * Example
- *      push b"I am the walrus.";
- *      hash_sha256;
- *      push 0x475b03e74f7ee448273dbde5ab892746c7b23a2b4d050ccb7d9270b6fb152b72;
- *      check_equal;
- *      assert_true;
- *
- *  Example
- *      setup {
- *          push b"I am the walrus.";
- *      }
- *      challenge {
- *          hash_sha256;
- *          push 0x475b03e74f7ee448273dbde5ab892746c7b23a2b4d050ccb7d9270b6fb152b72;
- *          check_equal;
- *          assert_true;
- *      }
- */
+// #[cfg(test)]
+// use hex_literal::hex;
+
+// /**
+//  * Types:
+//  * - UnsignedInteger
+//  * - Integer
+//  * - Float64
+//  * - Bytes(N)
+//  * - Multibase
+//  * - Multihash
+//  * - Multiaddr
+//  * - KeyTypes={Ed25519,Secp256k1,Secp256r1,Bls12_381}
+//  * - PublicKey(KeyType)
+//  * - PrivateKey(KeyType)
+//  * - JWT
+//  * - JWS
+//  * - JWE
+//  * - LDP
+//  * - JSON
+//  * - CBOR
+//  *
+//  * Functions
+//  * - Sign :: Bytes(N) -> PrivateKey(KeyType) => Bytes(SignatureSize[KeyType])
+//  * - VerifySignature :: Bytes(N) -> Bytes(SignatureSize[KeyType]) -> PublicKey(KeyType) => Boolean
+//  * - VerifyRecoveredSignature :: Bytes(N) -> Bytes(SignatureSize[KeyType]) => Boolean
+//  * - HashSha3_256 :: Bytes(N) => Bytes(32)
+//  * - Equal
+//  * - AssertTrue
+//  *
+//  * Example
+//  *      push b"I am the walrus.";
+//  *      hash_sha256;
+//  *      push 0x475b03e74f7ee448273dbde5ab892746c7b23a2b4d050ccb7d9270b6fb152b72;
+//  *      check_equal;
+//  *      assert_true;
+//  *
+//  *  Example
+//  *      setup {
+//  *          push b"I am the walrus.";
+//  *      }
+//  *      challenge {
+//  *          hash_sha256;
+//  *          push 0x475b03e74f7ee448273dbde5ab892746c7b23a2b4d050ccb7d9270b6fb152b72;
+//  *          check_equal;
+//  *          assert_true;
+//  *      }
+//  */
 
 fn sha256(input: &Vec<u8>) -> GenericArray<u8, U32> {
     // create a Sha256 object
@@ -68,27 +71,28 @@ fn sha256(input: &Vec<u8>) -> GenericArray<u8, U32> {
     return result;
 }
 
-fn sha3_256(input: &Vec<u8>) -> GenericArray<u8, U32> {
-    // create a Sha256 object
-    let mut hasher = Sha3_256::new();
-
-    // write input message
-    hasher.update(input);
-
-    // read hash digest and consume hasher
-    let result = hasher.finalize();
-    return result;
-}
-
-fn drop_bytes(n: usize, input: &Vec<u8>) -> Vec<u8> {
-    let mut result = input.clone();
-    result.drain(..n);
-    return result;
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
+    use hex_literal::hex;
+
+    fn sha3_256(input: &Vec<u8>) -> GenericArray<u8, U32> {
+        // create a Sha256 object
+        let mut hasher = Sha3_256::new();
+
+        // write input message
+        hasher.update(input);
+
+        // read hash digest and consume hasher
+        let result = hasher.finalize();
+        return result;
+    }
+
+    fn drop_bytes(n: usize, input: &Vec<u8>) -> Vec<u8> {
+        let mut result = input.clone();
+        result.drain(..n);
+        return result;
+    }
 
     #[test]
     fn test_sha2() {
@@ -117,9 +121,10 @@ mod tests {
         );
     }
 
-    #[test]
-    fn test_drop_bytes() {
-        let result = drop_bytes(6, &b"hello world".to_vec());
-        assert_eq!(&result[..], b"world");
-    }
+    // UNUSED
+    // #[test]
+    // fn test_drop_bytes() {
+    //     let result = drop_bytes(6, &b"hello world".to_vec());
+    //     assert_eq!(&result[..], b"world");
+    // }
 }
--- src/types.rs
+++ src/types.rs
@@ -12,6 +12,19 @@ use serde::{de, Deserialize, Serialize};
 use serde::de::{Deserializer};
 use serde::ser::{Serializer};
 
+
+// TODO:
+// - restack:
+//     + add common stack manipulation constructors
+//     + test against common stack manipulations, e.g. swap; swap = id
+
+// - json
+//     + add to stack type
+//     + add construction/destruction primitives
+//     + add property based tests
+
+
+
 fn serialize_generic_array_u8_u32<T, S>(v: &T, serializer: S) -> Result<S::Ok, S::Error>
 where
     T: AsRef<[u8]>,
@@ -79,10 +92,25 @@ pub type Stack = Vec<Elem>;
 #[derive(Debug, PartialEq, Serialize, Deserialize)]
 pub struct Restack {
     restack_depth: StackIx,
-    restack_vec: Vec<StackIx>
+    restack_vec: Vec<StackIx>,
 }
 
 impl Restack {
+    pub fn id() -> Self {
+        Restack {
+            restack_depth: 0,
+            restack_vec: vec![],
+        }
+    }
+
+    pub fn swap() -> Self {
+        Restack {
+            restack_depth: 2,
+            restack_vec: vec![1usize, 0],
+        }
+    }
+
+
     pub fn run(&self, stack: &Stack) -> Result<Stack, RestackError> {
         if self.restack_depth <= stack.len() {
             self.restack_vec.iter().map(|&restack_index|
@@ -90,7 +118,8 @@ impl Restack {
                     None => Err(RestackError::StackIndexInvalid{ restack_index: restack_index, restack_depth: self.restack_depth, }),
                     Some(stack_element) => Ok( stack_element.clone() ),
                 }
-            ).collect::<Result<Stack, RestackError>>()
+            ).collect::<Result<Stack, RestackError>>()?.extend(stack.drain(self.restack_depth..))
+
         } else {
             Err(RestackError::InvalidDepth{ stack_len: stack.len(), restack_depth: self.restack_depth, })
         }
@@ -104,7 +133,7 @@ impl Restack {
     }
 
     // x.append(y).run(s) == x.run(y.run(s))
-    pub fn append(&self, other: Restack) -> Restack {
+    pub fn append(&self, other: Self) -> Self {
         Restack {
             restack_depth: cmp::max(self.restack_depth, other.restack_depth),
             restack_vec: self.restack_vec.iter().map(|&restack_index|
@@ -119,7 +148,7 @@ impl Restack {
 }
 
 
-#[derive(Debug, Error)]
+#[derive(Debug, PartialEq, Error)]
 pub enum RestackError {
     #[error("invalid Restack: restack_index = {restack_index:?} out of bounds for restack_depth = {restack_depth:?}")]
     StackIndexInvalid {
@@ -135,5 +164,52 @@ pub enum RestackError {
 
 
 pub type Instructions = Vec<Instruction>;
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_id_restack() {
+        let example_stack = vec![Elem::Bool(false), Elem::Bool(true)];
+        assert_eq!(Ok(example_stack.clone()), Restack::id().run(&example_stack))
+
+    }
+
+    // #[test]
+    // fn test_sha2() {
+    //     let result = sha256(&b"hello world".to_vec());
+    //     assert_eq!(
+    //         result[..],
+    //         hex!(
+    //             "
+    //         b94d27b9934d3e08a52e52d7da7dabfac484efe37a5380ee9088f7ace2efcde9
+    //     "
+    //         )[..]
+    //     );
+    // }
+
+    // #[test]
+    // fn test_sha3() {
+    //     let result = sha3_256(&b"hello world".to_vec());
+    //     // println!("{:x?}", hex_encode(result.as_slice()));
+    //     assert_eq!(
+    //         result[..],
+    //         hex!(
+    //             "
+    //         644bcc7e564373040999aac89e7622f3ca71fba1d972fd94a31c3bfbf24e3938
+    //     "
+    //         )[..]
+    //     );
+    // }
+
+    // #[test]
+    // fn test_drop_bytes() {
+    //     let result = drop_bytes(6, &b"hello world".to_vec());
+    //     assert_eq!(&result[..], b"world");
+    // }
+
+}
 
 
